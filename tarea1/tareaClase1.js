4df282af67ff8ea4cf19f0cb69fbdba6e8c6e44b
lnlindao <lnlindao@hotmail.com>
2021-09-20 15:29:02 -0500

    tarea 1 js

---
+++ tarea1/tareaClase1.js
@@ -0,0 +1,6 @@
+var numero1 = prompt("Ingrese número 1:");
+var numero2 = prompt("Ingrese número 2:");
+var numero3 = 100;
+var resultado = parseFloat(numero1) * parseInt(numero2) * numero3;
+console.log("Respuesta:" + resultado);
+alert("Respuesta: " + resultado);
