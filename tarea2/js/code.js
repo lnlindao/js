44b12abf47d12058f99cf16abcdebb3eef70302c
lnlindao <lnlindao@hotmail.com>
2021-09-20 20:35:04 -0500

    tarea 2

---
+++ tarea2/js/code.js
@@ -0,0 +1,12 @@
+
+/** MOSTRAR EL NÚMERO SI ESTÁ ENTRE 10 Y 50 */
+
+let numero = parseInt( prompt("ingrese número:") );
+
+if ( numero >= 10 && numero <= 50){
+    alert("Número: "+ numero + " está entre 10 y 50");
+}
+else{
+    alert("Número: "+ numero  + " no está entre 10 y 50")
+    
+}
