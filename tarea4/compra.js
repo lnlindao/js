ae4795570fd04e158956654c9c78efcf44b93f04
lnlindao <lnlindao@hotmail.com>
2021-09-29 21:35:13 -0500

    tarea 4 agregada

---
+++ tarea4/compra.js
@@ -0,0 +1,41 @@
+/* LISTA DE COMPRAS 
+1. 2 lib Arroz - $ 2
+    2. 1 lib de azúcar - $ 1.5
+    3. 1 lib de sal - $ 1
+    4. 1 Pan de molde integral - $ 2.45
+    */
+
+var arroz2lib = 2, azucar1lib = 1.5, sal1lib = 1, moldeIntegral = 2.45, sumaCompra = 0;
+
+function comprar () { 
+    let item = parseInt(prompt("ingrese el número del item a comprar"));
+    compra ( item );
+}
+
+function compra ( itemCompra ){
+    if ( itemCompra == 1 ){
+        alert("Ud ha añadido 2 lib Arroz");
+        sumaCompras (arroz2lib);
+    } else if ( itemCompra == 2 ) {
+        alert("Ud ha añadido 1 lib de azúcar");
+        sumaCompras (azucar1lib);
+    } else if ( itemCompra == 3 ) {
+        alert("Ud ha añadido 1 lib de sal");
+        sumaCompras (sal1lib);
+    } else if ( itemCompra == 4 ) {
+        alert("Ud ha añadido 1 Pan de molde integral");
+        sumaCompras (moldeIntegral);
+    } else {
+        alert("Dato incorrecto")
+    }
+}
+
+function sumaCompras (producto){
+    sumaCompra = sumaCompra + producto;
+    console.log(sumaCompra);
+}
+
+
+function totalCompra(){
+    alert("El total de su compra es de $ " + sumaCompra)
+}
